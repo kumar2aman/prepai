9a46ebd19e4dd72b5f4294a81bfc44144cff0fdc
AMAN SINGH <2amansingh07@gmail.com>
2025-10-22 23:44:46 +0530

    updated routes endpoints

--- apps/backend/src/routes/routes.ts
+++ apps/backend/src/routes/routes.ts
@@ -1,13 +1,20 @@
 import { Router } from "express";
 
 import { ephemeralTokenRouter } from "../api/v1/ephemeral-token.js";
-import { convertAudioRouter } from "../api/v1/convert-audio.js";
+
 import { userinfoRouter } from "../api/v1/userinfo/userinfo.js";
+import {  geminiAudioRouter } from "../api/v1/GeminiAudio.js";
+import { chatHistoryRouter } from "../api/v1/chatHistory.js";
+import { userDataRouter } from "../api/v1/userData.js";
+import { userAudioRouter } from "../api/v1/userAudio.js";
 
 const router: Router = Router();
 
 router.use("/token", ephemeralTokenRouter);
-router.use("/convertAudio", convertAudioRouter);
+router.use("/useraudio", userAudioRouter);
+router.use("/geminiaudio", geminiAudioRouter);
 router.use("/userinfo", userinfoRouter);
+router.use("/chathistory",chatHistoryRouter) ;
+router.use("/userdata",userDataRouter)
 
 export { router };
