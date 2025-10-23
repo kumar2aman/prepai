d407703f3fbaffe2798df10870695bdad75547da
AMAN SINGH <2amansingh07@gmail.com>
2025-10-23 23:30:50 +0530

    updated midddleware  endpoints

--- apps/backend/src/auth-middleware.ts
+++ apps/backend/src/auth-middleware.ts
@@ -6,18 +6,16 @@ export function authMiddleware(req: Request, res: Response, next: NextFunction) 
 
   const token = req.cookies.token;
 
-  console.log("req.cookies.token", req.cookies.token);
-
   if (!token) {
     return res.status(401).json({ error: "Unauthorized" });
   }
 
   try {
     const decodedToken = jwt.verify(token as string, process.env.JWT_SECRET!);
-    console.log("decodedToken", decodedToken);
+  
     req.userId = (decodedToken as unknown as JwtPayload)
       .userid as unknown as string;
-    console.log("req.userId", req.userId);
+   
     next();
   } catch (error) {
     return res.status(401).json({ error: "Invalid token" });
