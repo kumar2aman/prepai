9e314cfa495f9b2c5f3472dd3df1fe53a4e8eb32
AMAN SINGH <2amansingh07@gmail.com>
2025-11-09 22:27:38 +0530

    added queue.ts for convert STT

---
+++ apps/backend/src/lib/queue.ts
@@ -0,0 +1,22 @@
+import { convertToText } from "../utils/assemblyaiSTT.js";
+
+const queue: string[] = [];
+
+let isProcessing = false;
+
+export function enqueue(chunk: string) {
+  queue.push(chunk);
+  if (!isProcessing) return processQueue();
+}
+
+async function processQueue() {
+  isProcessing = true;
+  while (queue.length > 0) {
+    const chunk = queue.shift();
+    if (!chunk) return "chunk is undefined";
+    const response = await convertToText({ data: chunk }); // await ensures no overlap
+    if(!response) return "response is undefined";
+    return response;
+  }
+  isProcessing = false;
+}
