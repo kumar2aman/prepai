a61b710d48e7111b4455c9d820c86d58792c6730
AMAN SINGH <2amansingh07@gmail.com>
2026-01-12 23:33:48 +0530

    removed audio parser middelware

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,23 +1,11 @@
 import express from "express";
-import {createServer} from "http";
+
 import cors from "cors";
 import cookiesParser from "cookie-parser";
 import { router } from "./routes/routes.js";
 import { authRouter } from "./api/v1/auth/controller.js";
-import { WebSocketServer } from "ws";
-import { newConnection } from "./api/v1/ws.userAudio.js";
-
-
 
 const app = express();
-
- const server = createServer(app);
-
-
-export const wss = new WebSocketServer({ server,path:"/ws" });
-
-
-wss.on("connection", newConnection);
 
 // middleware
 app.use(
@@ -28,12 +16,6 @@ app.use(
 );
 
 app.use(express.json({ limit: "50mb" }));
-app.use(
-  express.raw({
-    type: "audio/webm",
-    limit: "50mb",
-  })
-);
 
 app.use(cookiesParser());
 
@@ -41,11 +23,7 @@ app.use(cookiesParser());
 app.use("/api/v1", router);
 app.use("/api/v1/auth", authRouter);
 
-
-
-
-
 // run server
-server.listen(3001, () => {
+app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
