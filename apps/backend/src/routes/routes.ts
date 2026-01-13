c01e55877fc97239ed0f5434226a7dc2a11c4555
AMAN SINGH <2amansingh07@gmail.com>
2026-01-13 23:44:23 +0530

    updated routes

--- apps/backend/src/routes/routes.ts
+++ apps/backend/src/routes/routes.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 
 import { ephemeralTokenRouter } from "../api/v1/ephemeral-token.js";
 
-import { userinfoRouter } from "../api/v1/userinfo/userinfo.js";
-import {  geminiAudioRouter } from "../api/v1/GeminiAudio.js";
+import { userinfoRouter } from "../api/v1/userinfo/userInfo.js";
+import {  geminiAudioRouter } from "../api/v1/geminiAudio.js";
 import { chatHistoryRouter } from "../api/v1/chatHistory.js";
 import { userDataRouter } from "../api/v1/userData.js";
 import { userAudioRouter } from "../api/v1/userAudio.js";
