64a2fb01dd4d97466d895eb8614cc85ecc610cea
AMAN SINGH <2amansingh07@gmail.com>
2026-01-15 21:56:42 +0530

    updated userdata

--- apps/backend/src/api/v1/userData.ts
+++ apps/backend/src/api/v1/userData.ts
@@ -3,37 +3,7 @@ import { Router } from "express";
 import { authMiddleware } from "../../auth-middleware.js";
 import prisma from "../../db.js";
 
-
 const router: Router = Router();
-
-router.post("/", authMiddleware, async (req, res) => {
-  const userId = req.userId;
-console.log("userId:",userId);
-  if (!userId) {
-    return res.status(401).json({ error: "Unauthorized" });
-  }
-  
-  console.log(req.body);
-
-
-  const userData = req.body;
-
-  console.log("userData:", userData);
-  try {
-    await prisma.userData.create({
-     
-      data: {
-         userId: userId,  
-        ...userData,
-      },
-    });
-
-    res.status(200).json({ message: "User data saved successfully" });
-  } catch (error) {
-     console.error("Prisma Create Error:", error);
-    res.status(500).json({ error: "Failed to save user data" });
-  }
-});
 
 router.get("/", authMiddleware, async (req, res) => {
   const userId = req.userId;
@@ -47,6 +17,10 @@ router.get("/", authMiddleware, async (req, res) => {
         id: userId,
       },
     });
+
+    if (!user) {
+      return res.status(404).json({ error: "User data not found" });
+    }
 
     res.status(200).json(user);
   } catch (error) {
