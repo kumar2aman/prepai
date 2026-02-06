f9842b61b944c5ead66e342fb51eca479e9a76bc
AMAN SINGH <2amansingh07@gmail.com>
2026-02-06 23:32:24 +0530

    added userdata

--- apps/backend/src/api/v1/session.ts
+++ apps/backend/src/api/v1/session.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { authMiddleware } from "../../auth-middleware.js";
 import prisma from "../../db.js";
+import { result } from "../../lib/review.js";
 
 const router: Router = Router();
 
@@ -20,8 +21,7 @@ router.post("/createsession", authMiddleware, async (req, res) => {
     await prisma.session.create({
       data: {
         userId: userId,
-        totalSession: 1, //  total session is hard coded for now..
-        name: data as string,
+        name: data.name as string,
       },
     });
 
@@ -29,6 +29,37 @@ router.post("/createsession", authMiddleware, async (req, res) => {
   } catch (error) {
     console.error("Prisma Create Error:", error);
     res.status(500).json({ error: "Failed to create session" });
+  }
+});
+
+router.post("/create_sessiondata", authMiddleware, async (req, res) => {
+
+  const userId = req.userId;
+ 
+  const data = await result(); //  TODO: Needed to be tested before creation of session data
+  console.log("data:", data);
+  if (!userId) {
+    return res.status(401).json({ error: "Unauthorized" });
+  }
+
+  if (!data) {
+    return res.status(400).json({ error: "No data received." });
+  }
+
+  try {
+    await prisma.userData.update({
+      where: {
+        userId: userId,
+      },
+      data: {
+        ...data,
+      },
+    });
+
+    res.status(200).json("user data created");
+  } catch (error) {
+    console.error("Prisma Create Error:", error);
+    res.status(500).json({ error: "Failed to create user data" });
   }
 });
 
