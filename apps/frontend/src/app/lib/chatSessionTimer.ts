9eaee3684506a99fd04e86776d5d3192b1b76764
AMAN SINGH <2amansingh07@gmail.com>
2026-02-17 22:41:46 +0530

    fix bug timer start without user actions

--- apps/frontend/src/app/lib/chatSessionTimer.ts
+++ apps/frontend/src/app/lib/chatSessionTimer.ts
@@ -7,33 +7,31 @@ interface SessionTimerProps {
   isActive?: boolean;
 }
 
-
-const useSessionTimer = ({initialTimeoutSeconds, redirectPath,isActive}: SessionTimerProps) => {
+const useSessionTimer = ({
+  initialTimeoutSeconds,
+  redirectPath,
+  isActive,
+}: SessionTimerProps) => {
   // Initialize state with the total number of seconds
 
   const [timeLeft, setTimeLeft] = useState(initialTimeoutSeconds);
   const router = useRouter();
 
   useEffect(() => {
-
-    if(!isActive){
-      return
+    if (!isActive) {
+      return;
     }
-  
 
-
-  
-
-    // Set  interval 
+    // Set  interval
     const intervalId = setInterval(() => {
       // Decrease the time left every second
       setTimeLeft((prevTime) => prevTime - 1);
     }, 1000);
 
     // Cleanup function
-    
+
     return () => clearInterval(intervalId);
-  }, [timeLeft, redirectPath, navigator,isActive]); // Dependencies: Re-run when time runs out or redirect path changes
+  }, [timeLeft, redirectPath, navigator, isActive]); // Dependencies: Re-run when time runs out or redirect path changes
 
   // Function to reset the timer (e.g., when the user interacts with the app)
   const resetTimer = () => {
