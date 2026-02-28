95627e16bfae8c92d6206a3e0942de1da946b327
AMAN SINGH <2amansingh07@gmail.com>
2026-02-28 21:29:13 +0530

    added prismaClient

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -5,13 +5,7 @@ const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL,
 });
 
-<<<<<<< HEAD
-import { PrismaClient } from "@prisma/client";
-
-export const prisma = new PrismaClient();
-export * from "@prisma/client";
-=======
 export const prisma = new PrismaClient({
   adapter,
 });
->>>>>>> ed7ff94 (fixed prisma not working)
+
