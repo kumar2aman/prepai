8183470e382abd2f1d0684fb54e583a4123125b4
AMAN SINGH <2amansingh07@gmail.com>
2026-03-02 12:13:03 +0530

    fixed production cookies are being blocked

--- apps/backend/src/api/v1/auth/controller.ts
+++ apps/backend/src/api/v1/auth/controller.ts
@@ -105,7 +105,7 @@ router.post("/logout", async (req, res) => {
   res.clearCookie("token", {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     path: "/",
   });
   res.status(200).json({ message: "Logout successful" });
