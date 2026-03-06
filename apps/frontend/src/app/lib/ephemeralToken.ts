78d92fef174733d12178df88e1c0c9f42fae6d89
AMAN SINGH <2amansingh07@gmail.com>
2026-03-06 17:43:45 +0530

    fix:token not found error

--- apps/frontend/src/app/lib/ephemeralToken.ts
+++ apps/frontend/src/app/lib/ephemeralToken.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 
 export async function getEphemeralToken(): Promise<string | null> {
   try {
-    const res = await axios.get(`${process.env.backend_URL}/api/v1/token`,{
+    const res = await axios.get(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/token`,{
       withCredentials: true,
     });
     console.log("Ephemeral token:", res.data);
