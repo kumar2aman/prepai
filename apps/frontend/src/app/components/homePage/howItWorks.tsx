380eb11677f4b69a456a4caa1f9e24969d0e535d
AMAN SINGH <2amansingh07@gmail.com>
2026-07-08 08:55:11 +0530

    feat(video): added video of how it works

--- apps/frontend/src/app/components/homePage/howItWorks.tsx
+++ apps/frontend/src/app/components/homePage/howItWorks.tsx
@@ -16,11 +16,19 @@ function HowItWorks() {
             <h1 className='font-ubuntu font-medium text-6xl mb-6 text-center'>
                 How <span className='gradient-blue  text-transparent bg-clip-text'>PrepAi</span>  Works 
             </h1>
-            <Button size={"xl"} variant={'outline'} className='w-42 mt-12 mb-7 font-sans text-2xl hover:bg-gray-900 cursor-pointer'>
-                Play <Play className='size-xl text-orange-400'/>
-            </Button>
-            <h1 className='font-sans text-xl font-semibold  text-red-500'>Coming Soon !!</h1>
-        </div>
+          <video
+  autoPlay
+  
+  playsInline
+  loop
+  controls
+  width="70%"
+  height="700">
+  <source src="https://res.cloudinary.com/or2qjmvk/video/upload/v1783455515/InShot_compress_nemt7f.mp4" type="video/mp4"/>
+</video>
+
+           </div>
+        
     </motion.div>
   )
 }
